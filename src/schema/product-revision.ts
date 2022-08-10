de2d108f4fef310e17b3b78a80ce0270aca1101f
charlocharlie <charlielaabs@gmail.com>
2022-08-09 23:27:24 -0500

    Fix product revision schema issue

--- src/schema/product-revision.ts
+++ src/schema/product-revision.ts
@@ -1,17 +1,14 @@
 import mongoose from 'mongoose';
 import { IProduct, productSchema } from './product';
 
+const revisionSchema = productSchema.obj;
+delete revisionSchema._id;
+
 // Copy product schema so we can add the index
-const productRevisionSchema = new mongoose.Schema<IProduct>(
-  {
-    ...productSchema.obj,
-    _id: mongoose.Types.ObjectId,
-  },
-  {
-    timestamps: true, // Generate createdAt and updatedAt timestamps
-    minimize: false, // Preserve empty objects ({})
-  }
-);
+const productRevisionSchema = new mongoose.Schema<IProduct>(revisionSchema, {
+  timestamps: true, // Generate createdAt and updatedAt timestamps
+  minimize: false, // Preserve empty objects ({})
+});
 
 productRevisionSchema.index({ productId: 1, createdAt: 1 }, { unique: true });
 
